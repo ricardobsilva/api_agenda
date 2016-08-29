9813ad344ea878aff413869577e7eb50b40dfa2b
ricardosilva13 <ricardo.b.silva13@gmail.com>
2016-08-28 22:23:46 -0300

    add resource create contact

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -5,4 +5,18 @@ class Api::V1::ContactsController < ApplicationController
     render json: @api_v1_contacts
   end
 
+  def create
+    @api_v1_contact = Contact.new(contact_params)
+    
+    if @api_v1_contact.save
+      render json: @api_v1_contact
+    else
+      render json: @api_v1_contact.errors
+    end
+  end
+
+  def contact_params
+    params.require(:contact).permit(:name, :birth, :name, :mobile_operator_id)
+  end
+
 end
